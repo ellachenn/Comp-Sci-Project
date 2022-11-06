0608ebbdbe312cdf80b148fde217ac7869dc80aa
Ella Chen <ella_chen@cate.org>
2022-11-05 20:41:29 -0700

    homework

--- sketch.js
+++ sketch.js
@@ -1,15 +1,29 @@
+// control "c" to get back to where you type "npx http-server" in terminal
+
 function setup() {
-    createCanvas(400, 400);
-    console.log("Hello World")
+  createCanvas(400, 400);
+  console.log("Hello World")
+
+  input = createInput();
+  input.position(20, 65);
+
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+
+  greeting = createElement('h2', 'input exercpt from book');
+  greeting.position(20, 5);
+
+  textAlign(CENTER);
+  textSize(50);
+}
+
+function greet() {
+  const exerpt = input.value();
+  console.log(exerpt)
 }
   
-function draw() {
-  background(0, 255, 0);
-}
 
-let booktext = prompt("Enter exerpt from book: ");
-// how do you extract a response from user input from prompt?
-// booktext = parseFloat(booktext);
-console.log(booktext);
-
-// new code
+// function draw() {
+//   background(0, 255, 0);
+// }
