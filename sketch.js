f72762d22ba6862e39a9da635b99806b9ec4488c
Ella Chen <ella_chen@cate.org>
2022-11-13 15:44:37 -0800

    hw

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 
 // control "c" to get back to where you type "npx http-server" in terminal
 
-let result;
+let wordList;
+let listArray = [];
+let exerptArray = [];
 
 function preload() {
-  result = loadStrings('grades.txt');
+  wordList = loadStrings('grades.txt');
 }
 
 function setup() {
@@ -35,31 +37,48 @@ function convertToArray() {
   console.log(exerptArray)
 }
 
-// idea 2
-function compare() {
-  // let counter = 0
-  for (i of result) {
-    (word + i) = result[i].split(",");
+
+// idea 2: sorting exerpt word into array
+
+// need a counter for each different grade  
+// or instead of counter, create a 2d array that has an array for each grade, and i push each exerpt word into the corresponding grade array within the 2d array
+// bc then i can just do array[i].length to count how many words are in each grade level and wouldn't have to have 6 different counters
+// example to making and initializing a 2d array:
+// https://replit.com/@advanced-cs-2022-23/55-2D-Array-EllaChen5#script.js
+
+// fills out listArray, soring grade words into a 2D array comrpised of each grade's words separately
+function listToArray() {
+  for (i of wordList) {
+    (word + i) = wordList[i].split(",");
     for (word of (word + i)) {
-      if (word == exword) {
-        
-        // need a counter for each different grade  
-        // or instead of counter, create a 2d array that has an array for each grade, and i push each exerpt word into the corresponding grade array within the 2d array
-        // bc then i can just do array[i].length to count how many words are in each grade level and wouldn't have to have 6 different counters
-        // example to making and initializing a 2d array:
-        // https://replit.com/@advanced-cs-2022-23/55-2D-Array-EllaChen5#script.js
+      for (let r = 0; r < 6; r++) {
+        listArray[r] = [];
+        for (let c = 0; c < listArray[r].length; c++) {
+          listArray[r][c].push(word);
+        }
+      }
+    }
+  }
+}
+
+function compare() {
+  for (character in exerptArray) {
+    for (grade in listArray) {
+      for (word in grade) {
+        if (character == word) {
+          // push character into the appropriate grade array in exerptArray
+        }
       }
     }
   }
 }
 
 
-
 // idea 1
 // function make2DArray() {
 //   let twoDArray = []
-//   for (let i of result) {
-//     let words = result[i].split(",");
+//   for (let i of wordList) {
+//     let words = wordList[i].split(",");
 //     for (w of words) {
 //       twoDArray.push(w);
 //     }
@@ -67,28 +86,12 @@ function compare() {
 //   console.log(twoDArray);
 // }
 
-// idea 1
-// function compare() {
-//   counter = 0
-//   for (let grade of twoDArray) {
-//     for (let word of grade) {
-//       for (let exword of exerptArray) {
-//         if (word == exword) {
-//           // need a counter for each different grade
-//           counter = counter + 1
-//         }
-//       }
-//     }
-//   }
-//   exerptArray
-// }
-
 
 
 
 // getting at individual words in the grades.txt file
-// word + i = result[i].split(",");
-// let words = result[0].split(",");
+// word + i = wordList[i].split(",");
+// let words = wordList[0].split(",");
 // console.log(words[0]);
 
 // function draw() {
